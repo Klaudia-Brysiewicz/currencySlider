59f8b923c82bc9681ab9fa11966d603c976b4903
Klaudia <brysiewiczk@gmail.com>
2019-11-28 15:14:52 +0100

    slider - config && project setup with typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>currencyExchange</h2>
+      </header>
+    </div>
+  );
+};
+
+export default App;
